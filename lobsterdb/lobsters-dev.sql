7675fd50f111160b8d34ef3121707465912531a6
Kyle Isom <coder@kyleisom.net>
2012-08-30 23:48:00 -0600

    sql code to create a test db on a dev sys

---
+++ lobsterdb/lobsters-dev.sql
@@ -0,0 +1,14 @@
+-- uses default username and database, i.e. kyle@kyle
+--create role golobs 
+--      login 
+--      password '';
+--create database lobsters 
+--                ENCODING = 'UTF8' 
+--                LC_COLLATE = 'en_US.UTF-8' 
+--                LC_CTYPE = 'en_US.UTF-8' 
+--                template = template0;
+--ALTER DATABASE lobsters OWNER TO golobs ;
+--\connect lobsters
+create table posted (guid text unique not null, 
+                     posted boolean default false not null);
+create index posted_idx on posted;
