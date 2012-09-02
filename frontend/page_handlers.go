39ea94cfed72f062f579afca7dde762d84822662
Kyle Isom <coder@kyleisom.net>
2012-09-02 11:51:07 -0600

    add story count to page

--- frontend/page_handlers.go
+++ frontend/page_handlers.go
@@ -3,10 +3,12 @@ package frontend
 import (
 	"fmt"
 	"golobsters/bot"
+	"golobsters/lobsterdb"
 	"net/http"
 )
 
 func rootPage(w http.ResponseWriter, req *http.Request) {
 	page := "last update: " + bot.LastUpdate()
+	page += fmt.Sprintf("\nstories posted: %d\n", lobsterdb.CountStories)
 	fmt.Fprintln(w, page)
 }
