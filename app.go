5fcd8014f101bf68abf853b38a92c2a3bac1dc7f
Kyle Isom <coder@kyleisom.net>
2012-09-01 22:15:50 -0600

    set up basic functionality for app

--- app.go
+++ app.go
@@ -7,13 +7,23 @@ package main
 
 import (
 	"fmt"
-        "golobsters/bot"
-	"golobsters/lobsterdb"
+	"github.com/gokyle/gomon/monitor"
+	"golobsters/bot"
+	"log"
 	"os"
+	"time"
 )
 
 // run is stubbed
 func run() {
+	go monitor.Monitor(bot.Run)
+	if "" == bot.LastUpdate() {
+		log.Println("[+] bot no updates yet")
+	} else {
+		log.Println("[+] bot last update: ", bot.LastUpdate)
+	}
+	time.Sleep(5 * 1000 * time.Millisecond)
+	log.Println("[+] bot last update: ", bot.LastUpdate)
 	return
 }
 
@@ -29,7 +39,7 @@ func validate_env_var(name string) bool {
 func validate_environment() {
 	vars := []string{"TW_CKEY", "TW_CSEC", "TW_ATOK", "TW_ASEC",
 		"PG_DBNAME", "PG_USER", "PG_PASS", "PG_HOST",
-		"PG_PORT", "PG_SSL"}
+		"PG_PORT", "PG_SSLMODE"}
 	for _, name := range vars {
 		if !validate_env_var(name) {
 			panic(fmt.Sprintf("missing environment variable: %s",
@@ -38,7 +48,15 @@ func validate_environment() {
 	}
 }
 
+func init() {
+	validate_environment()
+	err := monitor.ConfigFromJson()
+	if err != nil {
+		fmt.Println("[!] error configuring monitor: ", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-	fmt.Println(lobsterdb.ConnStringFromEnv())
 	run()
 }
