f7c53d71206a87c57bbe54c56370087b77eac2fb
Kyle Isom <coder@kyleisom.net>
2012-09-02 18:39:43 -0600

    update front end page; ADD TODO

--- frontend/page_handlers.go
+++ frontend/page_handlers.go
@@ -8,7 +8,11 @@ import (
 )
 
 func rootPage(w http.ResponseWriter, req *http.Request) {
-	page := "last update: " + bot.LastUpdate()
+        page := "twitter account: @lobsternews\n"
+        page += "git repo: git clone git://github.com/gokyle/golobsters.git\n"
+        page += "github page: http://gokyle.github.com/golobsters/\n\n"
+        page += "stats\n=====\n"
+	page += "last update: " + bot.LastUpdate()
 	page += fmt.Sprintf("\nstories posted: %d\n", lobsterdb.CountStories())
 	fmt.Fprintln(w, page)
 }
